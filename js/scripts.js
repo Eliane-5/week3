12bb9e1d223e637bb27fcb5e700ae62a87b60894
Eliane-5 <irankundaeliane9@gmail.com>
2019-07-26 19:09:43 +0200

    adding hovering over images using jquerry on images

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,104 @@ $(document).ready(function(){
         $("#des3").toggle();
         $("#img3").toggle();
     });
+    // $('.imgP').animate({
+    //     opacity:1
+
+    // });
+    $(".word1").hide();
+    $(".img1").animate({
+        opacity:1
+    });
+    $('.img1').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word1").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word1").fadeOut();
+    });
+    $(".word2").hide();
+    $(".img2").animate({
+        opacity:1
+    });
+    $('.img2').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word2").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word2").fadeOut();
+    });
+    $(".word3").hide();
+    $(".img3").animate({
+        opacity:1
+    });
+    $('.img3').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word3").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word3").fadeOut();
+    });
+    $(".word4").hide();
+    $(".img4").animate({
+        opacity:1
+    });
+    $('.img4').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word4").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word4").fadeOut();
+    });
+    $(".word5").hide();
+    $(".img5").animate({
+        opacity:1
+    });
+    $('.img5').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word5").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word5").fadeOut();
+    });
+    $(".word6").hide();
+    $(".img6").animate({
+        opacity:1
+    });
+    $('.img6').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word6").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word6").fadeOut();
+    });
+    $(".word7").hide();
+    $(".img7").animate({
+        opacity:1
+    });
+    $('.img7').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word7").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word7").fadeOut();
+    });
+    $(".word8").hide();
+    $(".img8").animate({
+        opacity:1
+    });
+    $('.img8').hover(function() {
+        $(this).stop().animate({opacity:0.4},200);
+        $(".word8").fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},200)
+        $(".word8").fadeOut();
+    });
 });
